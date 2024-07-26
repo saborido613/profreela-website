508e42b6ff788c91b44fa62b6105b3d70252853b
FelipeCarlota <felipe.carlota@gmail.com>
2024-07-26 14:44:17 -0300

    feat: criado componente para todas as sections

--- src/Components/Sections/SectionSobre/SectionSobre.tsx
+++ src/Components/Sections/SectionSobre/SectionSobre.tsx
@@ -1,4 +1,4 @@
-import useSectionRefs from "../../../hooks/useSectionRefs";
+import SectionComponent from "../../shared/component/Section/SectionComponent";
 import FotoSection1 from "/images/Scrum board-bro.png";
 
 export interface SectionProps {
@@ -7,40 +7,36 @@ export interface SectionProps {
 }
 
 function SectionSobre(props: SectionProps) {
-  const { sectionRefs } = useSectionRefs();
-
   return (
-    <section
-      className="flex flex-col"
+    <SectionComponent
       id={props.id}
-      ref={sectionRefs.current[props.id]}
-    >
-      <div className="flex justify-center w-full pt-8">
-        <h1 className="text-5xl font-extrabold">{props.title}</h1>
-      </div>
-      <div className="flex flex-col md:flex-row justify-center items-center container-[75vw] w-full md:container mx-auto pt-10">
-        <img src={FotoSection1} alt="" className="md:h-[70vh]" />
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam
-          vitae numquam commodi tempora, expedita eaque recusandae. Ipsum,
-          reprehenderit pariatur quae provident aliquid excepturi libero illum
-          voluptatem culpa optio? Sequi fugiat magnam consequatur maiores
-          ratione debitis, modi hic consectetur cupiditate quasi animi eaque est
-          nihil quis id dignissimos ea ipsum quisquam sapiente impedit pariatur
-          illum reiciendis. Expedita eius culpa ab quia repudiandae aliquid
-          iusto tempora hic rem! Sit itaque praesentium quod sed dolorum
-          dolores, reprehenderit, tempora ipsum inventore aliquid consequuntur
-          accusantium in excepturi cum veniam vel adipisci! Accusamus ex velit
-          deleniti quidem rerum a. Commodi consectetur eaque dolorum maxime
-          error velit, provident inventore nobis nam iste similique blanditiis
-          fuga, quis suscipit voluptas tempora numquam. Accusantium nulla non
-          officiis laudantium odit iure deleniti saepe cum ad reprehenderit eum
-          dignissimos reiciendis, quod culpa vitae iusto porro optio. Laboriosam
-          neque repellat deleniti, ipsum ab iusto assumenda vel illo hic,
-          officiis temporibus voluptas repellendus dolores.
-        </p>
-      </div>
-    </section>
+      title={props.title}
+      children={
+        <div className="flex flex-col md:flex-row justify-center items-center container-[75vw] w-full md:container mx-auto pt-10">
+          <img src={FotoSection1} alt="" className="md:h-[70vh]" />
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam
+            vitae numquam commodi tempora, expedita eaque recusandae. Ipsum,
+            reprehenderit pariatur quae provident aliquid excepturi libero illum
+            voluptatem culpa optio? Sequi fugiat magnam consequatur maiores
+            ratione debitis, modi hic consectetur cupiditate quasi animi eaque
+            est nihil quis id dignissimos ea ipsum quisquam sapiente impedit
+            pariatur illum reiciendis. Expedita eius culpa ab quia repudiandae
+            aliquid iusto tempora hic rem! Sit itaque praesentium quod sed
+            dolorum dolores, reprehenderit, tempora ipsum inventore aliquid
+            consequuntur accusantium in excepturi cum veniam vel adipisci!
+            Accusamus ex velit deleniti quidem rerum a. Commodi consectetur
+            eaque dolorum maxime error velit, provident inventore nobis nam iste
+            similique blanditiis fuga, quis suscipit voluptas tempora numquam.
+            Accusantium nulla non officiis laudantium odit iure deleniti saepe
+            cum ad reprehenderit eum dignissimos reiciendis, quod culpa vitae
+            iusto porro optio. Laboriosam neque repellat deleniti, ipsum ab
+            iusto assumenda vel illo hic, officiis temporibus voluptas
+            repellendus dolores.
+          </p>
+        </div>
+      }
+    />
   );
 }
 
