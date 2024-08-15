95a7ab08f5a715542b42706969d2bf65dfff2295
FelipeCarlota <felipe.carlota@gmail.com>
2024-08-15 12:12:54 -0300

    chore: adicionado página de erro

---
+++ src/Components/ErrorPage/index.tsx
@@ -0,0 +1,13 @@
+function ErrorPage() {
+  return (
+    <div className="h-[100vh] bg-purple-950 flex flex-col justify-center gap-10 text-white text-center">
+      <h1 className="font-black text-2xl">Erro 404</h1>
+      <p>
+        A página solicitada não foi encontrada. Por favor, contate o suporte
+        através do email: contatoprofreela@gmail.com
+      </p>
+    </div>
+  );
+}
+
+export default ErrorPage;
--- src/Routes/rotas.tsx
+++ src/Routes/rotas.tsx
@@ -3,19 +3,27 @@ import Path from "./paths";
 import Home from "../Components/Home/Home";
 import PoliticaPrivacidade from "../Components/PoliticaPrivacidade/PoliticaPrivacidade";
 import TermosCondicoes from "../Components/TermosCondicoes/TermosCondicoes";
+import ErrorPage from "../Components/ErrorPage";
 
 const router = createBrowserRouter([
   {
-    path: Path.POLITICA_PRIVACIDADE,
-    element: <PoliticaPrivacidade />
-  },
-  {
-    path: Path.TERMOS_E_CONDICOES,
-    element: <TermosCondicoes />
-  },
-  {
-    path: Path.HOME,
-    element: <Home />
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: Path.POLITICA_PRIVACIDADE,
+        element: <PoliticaPrivacidade />
+      },
+      {
+        path: Path.TERMOS_E_CONDICOES,
+        element: <TermosCondicoes />
+      },
+      {
+        path: Path.HOME,
+        element: <Home />
+      }
+    ]
   }
 ]);
 
