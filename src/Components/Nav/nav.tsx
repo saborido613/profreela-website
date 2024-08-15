0e19ef13dfb11571ba523b9a2d6aba65f045c8bc
FelipeCarlota <felipe.carlota@gmail.com>
2024-08-15 15:11:45 -0300

    chore: ajustado o navbar para pegar a tela toda com o container

--- src/Components/Nav/nav.tsx
+++ src/Components/Nav/nav.tsx
@@ -41,40 +41,38 @@ function Nav() {
   /* eslint-enable */
 
   return (
-    <nav className="w-full">
-      <nav className="bg-purple-600 fixed w-full">
-        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-          <div className="relative flex h-16 items-center justify-between">
-            <Logo />
-            <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
-              {/*  Mobile menu button */}
-              <button
-                ref={elementRef}
-                onClick={toggleMenu}
-                type="button"
-                className="relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-purple-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-                aria-controls="mobile-menu"
-                aria-expanded="false"
-              >
-                {isOpen ? <HiX /> : <HiMenu />}
-              </button>
-            </div>
+    <nav className="bg-purple-600 fixed w-full">
+      <div className="container mx-auto">
+        <div className="relative flex h-16 items-center justify-between">
+          <Logo />
+          <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
+            {/*  Mobile menu button */}
+            <button
+              ref={elementRef}
+              onClick={toggleMenu}
+              type="button"
+              className="relative inline-flex items-center justify-center rounded-md p-2 text-white hover:bg-purple-300 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+              aria-controls="mobile-menu"
+              aria-expanded="false"
+            >
+              {isOpen ? <HiX /> : <HiMenu />}
+            </button>
+          </div>
 
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                <Menus />
-              </div>
+          <div className="hidden sm:ml-6 sm:block">
+            <div className="flex space-x-4">
+              <Menus />
             </div>
           </div>
         </div>
+      </div>
 
-        {/* Mobile menu, show/hide based on menu state. */}
-        <div className={`${!isOpen && "hidden"}`} id="mobile-menu">
-          <div className="space-y-1 px-2 pb-3 pt-2">
-            <Menus isOpen />
-          </div>
+      {/* Mobile menu, show/hide based on menu state. */}
+      <div className={`${!isOpen && "hidden"}`} id="mobile-menu">
+        <div className="space-y-1 px-2 pb-3 pt-2">
+          <Menus isOpen />
         </div>
-      </nav>
+      </div>
     </nav>
   );
 }
