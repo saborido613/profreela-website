a4d3f95261b31c19a9035845347f2370082005fa
FelipeCarlota <felipe.carlota@gmail.com>
2024-08-15 12:39:32 -0300

    chore: voltando as rotas como estavam antes

--- src/Routes/rotas.tsx
+++ src/Routes/rotas.tsx
@@ -1,29 +1,21 @@
 import { createBrowserRouter } from "react-router-dom";
-import Path from "./paths";
 import Home from "../Components/Home/Home";
 import PoliticaPrivacidade from "../Components/PoliticaPrivacidade/PoliticaPrivacidade";
 import TermosCondicoes from "../Components/TermosCondicoes/TermosCondicoes";
-import ErrorPage from "../Components/ErrorPage";
+import Path from "./paths";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <Home />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: Path.POLITICA_PRIVACIDADE,
-        element: <PoliticaPrivacidade />
-      },
-      {
-        path: Path.TERMOS_E_CONDICOES,
-        element: <TermosCondicoes />
-      },
-      {
-        path: Path.HOME,
-        element: <Home />
-      }
-    ]
+    path: Path.POLITICA_PRIVACIDADE,
+    element: <PoliticaPrivacidade />
+  },
+  {
+    path: Path.TERMOS_E_CONDICOES,
+    element: <TermosCondicoes />
+  },
+  {
+    path: Path.HOME,
+    element: <Home />
   }
 ]);
 
