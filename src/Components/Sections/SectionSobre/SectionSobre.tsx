bfbe2a27c17ffc5798da9b928454e4df8475d53a
FelipeCarlota <felipe.carlota@gmail.com>
2024-08-15 11:50:58 -0300

    chore: ajustado o texto do sobre

--- src/Components/Sections/SectionSobre/SectionSobre.tsx
+++ src/Components/Sections/SectionSobre/SectionSobre.tsx
@@ -12,7 +12,8 @@ function SectionSobre(props: SectionProps) {
       id={props.id}
       title={props.title}
       children={
-        <div className="flex
+        <div
+          className="flex
         flex-col
         md:flex-row
         justify-center
@@ -21,28 +22,36 @@ function SectionSobre(props: SectionProps) {
         w-full
         md:container
         mx-auto
-        pt-10">
-          <img src={FotoSobre} alt="" className="md:h-[60vh] lg:h-[70vh]" />
-          <p className="my-4 text-justify">
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quibusdam
-            vitae numquam commodi tempora, expedita eaque recusandae. Ipsum,
-            reprehenderit pariatur quae provident aliquid excepturi libero illum
-            voluptatem culpa optio? Sequi fugiat magnam consequatur maiores
-            ratione debitis, modi hic consectetur cupiditate quasi animi eaque
-            est nihil quis id dignissimos ea ipsum quisquam sapiente impedit
-            pariatur illum reiciendis. Expedita eius culpa ab quia repudiandae
-            aliquid iusto tempora hic rem! Sit itaque praesentium quod sed
-            dolorum dolores, reprehenderit, tempora ipsum inventore aliquid
-            consequuntur accusantium in excepturi cum veniam vel adipisci!
-            Accusamus ex velit deleniti quidem rerum a. Commodi consectetur
-            eaque dolorum maxime error velit, provident inventore nobis nam iste
-            similique blanditiis fuga, quis suscipit voluptas tempora numquam.
-            Accusantium nulla non officiis laudantium odit iure deleniti saepe
-            cum ad reprehenderit eum dignissimos reiciendis, quod culpa vitae
-            iusto porro optio. Laboriosam neque repellat deleniti, ipsum ab
-            iusto assumenda vel illo hic, officiis temporibus voluptas
-            repellendus dolores.
-          </p>
+        pt-10"
+        >
+          <img
+            src={FotoSobre}
+            alt="Uma mulher na esquuerda apontando para um quadro kanbam e um homem na direita falando sobre a tarefa que ela está apontando. Na imagem possui um vaso no lado esquerdo da mulher e um armário na frente do homem. Ambos abaixo do quadro kanbam"
+            className="md:h-[60vh] lg:h-[70vh]"
+          />
+          <div>
+            <p className="my-4 text-justify">
+              Nós entendemos que um dos grandes desafios dos freelancers é a
+              organização das demandas para cumprir os prazos prometidos aos
+              clientes. Muitas vezes, nos deparamos com a incerteza sobre o
+              tempo necessário para concluir um projeto, perguntando: “Será que
+              vai dar tempo?”.
+            </p>
+            <p className="my-4 text-justify">
+              É por isso que estamos aqui, para ajudar. Com o ProFreela, você
+              pode organizar suas tarefas e estabelecer prazos mais realistas
+              para seus clientes. Além disso, nossa plataforma permite o envio
+              direto de itens para revisão, facilitando a comunicação e o
+              feedback.
+            </p>
+            <p className="my-4 text-justify">
+              Mas não paramos por aí. Sabemos que a organização financeira é tão
+              importante quanto a organização dos projetos. Por isso, o
+              ProFreela oferece um módulo financeiro que proporciona
+              previsibilidade nas suas entradas e saídas, ajudando você a manter
+              suas finanças em ordem.
+            </p>
+          </div>
         </div>
       }
     />
